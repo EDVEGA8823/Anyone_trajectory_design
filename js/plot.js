ac8d5ebb0e162595533c37e1f1f8dd85961bcd10
EDVEGA8823 <user.email>
2024-12-13 16:34:43 +0900

    軸をいい感じに変更

--- js/plot.js
+++ js/plot.js
@@ -31,11 +31,6 @@ const lines = [];
 const aLight = new THREE.AmbientLight(0xffffff, 1); // 環境光源
 scene.add(aLight);
 
-const labelRenderer = new THREE.CSS2DRenderer();
-labelRenderer.setSize(window.innerWidth, window.innerHeight);
-labelRenderer.domElement.style.position = 'absolute';
-labelRenderer.domElement.style.top = '0';
-
 // document.body.appendChild(labelRenderer.domElement);
 //球体
 // const sphere = new THREE.Mesh(
@@ -100,32 +95,31 @@ function updateLine(lineData, newPoints) {
 // 点を管理する配列
 const pointsArray = [];
 
-function createPlanets(){
+function createPlanets() {}
+const plot_area = document.getElementById("graph-panel");
 
-}
-
-const controls = new THREE.OrbitControls(camera, renderer.domElement);
+const labelRenderer = new THREE.CSS2DRenderer();
+labelRenderer.setSize(plot_area.clientWidth, plot_area.clientHeight);
+labelRenderer.domElement.style.position = "absolute";
+labelRenderer.domElement.style.top = "75px";
+plot_area.appendChild(labelRenderer.domElement);
+const controls = new THREE.OrbitControls(camera, labelRenderer.domElement);
 controls.enablePan = false;
 controls.maxDistance = 100;
 // 描画ループ
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-animate();
 
 const axis = [
   createLine(
     [new THREE.Vector3(-50, 0, 0), new THREE.Vector3(50, 0, 0)],
-    0xcccccc
+    0xaaaaaa
   ),
   createLine(
     [new THREE.Vector3(0, 50, 0), new THREE.Vector3(0, -50, 0)],
-    0xcccccc
+    0xaaaaaa
   ),
   createLine(
     [new THREE.Vector3(0, 0, -50), new THREE.Vector3(0, 0, 50)],
-    0xcccccc
+    0xaaaaaa
   ),
 ];
 const xticks0_1 = [];
@@ -144,19 +138,19 @@ for (let i = -5; i < 5; i = i + 0.1) {
   xticks0_1.push(
     createLine(
       [new THREE.Vector3(i, 0, -0.05), new THREE.Vector3(i, 0, 0.05)],
-      0xcccccc
+      0xaaaaaa
     )
   );
   yticks0_1.push(
     createLine(
       [new THREE.Vector3(0, i, -0.05), new THREE.Vector3(0, i, 0.05)],
-      0xcccccc
+      0xaaaaaa
     )
   );
   zticks0_1.push(
     createLine(
       [new THREE.Vector3(-0.05, 0, i), new THREE.Vector3(0.05, 0, i)],
-      0xcccccc
+      0xaaaaaa
     )
   );
 }
@@ -166,19 +160,19 @@ for (let i = -20; i < 20; i = i + 1) {
   xticks1.push(
     createLine(
       [new THREE.Vector3(i, 0, -0.2), new THREE.Vector3(i, 0, 0.2)],
-      0xcccccc
+      0xaaaaaa
     )
   );
   yticks1.push(
     createLine(
       [new THREE.Vector3(0, i, -0.2), new THREE.Vector3(0, i, 0.2)],
-      0xcccccc
+      0xaaaaaa
     )
   );
   zticks1.push(
     createLine(
       [new THREE.Vector3(-0.2, 0, i), new THREE.Vector3(0.2, 0, i)],
-      0xcccccc
+      0xaaaaaa
     )
   );
 }
@@ -187,23 +181,85 @@ for (let i = -50; i < 50; i = i + 5) {
   xticks5.push(
     createLine(
       [new THREE.Vector3(i, 0, -1), new THREE.Vector3(i, 0, 1)],
-      0xcccccc
+      0xaaaaaa
     )
   );
   yticks5.push(
     createLine(
       [new THREE.Vector3(0, i, -1), new THREE.Vector3(0, i, 1)],
-      0xcccccc
+      0xaaaaaa
     )
   );
   zticks5.push(
     createLine(
       [new THREE.Vector3(-1, 0, i), new THREE.Vector3(1, 0, i)],
-      0xcccccc
+      0xaaaaaa
     )
   );
 }
-console.log(axis);
+const ticks_labels = [];
+for (let i = -50; i < 50; i++) {
+  if (i == 0) continue;
+  if (i % 5 != 0 && (i < -10 || i > 10)) continue;
+  const ticks_label_x = document.createElement("div");
+
+  if (i % 5 != 0) ticks_label_x.className = "label_1au_x";
+  else ticks_label_x.className = "label_5au_x";
+  ticks_label_x.textContent = Math.abs(i) + "AU";
+  ticks_label_x.style.backgroundColor = "transparent";
+
+  axis[1].line.layers.enableAll();
+  const Label_x = new THREE.CSS2DObject(ticks_label_x);
+  if (i % 5 != 0) Label_x.position.set(i, 0, 0.3);
+  else Label_x.position.set(i, 0, 1.3);
+
+  axis[1].line.add(Label_x);
+  Label_x.layers.set(0);
+
+  const ticks_label_y = document.createElement("div");
+
+  if (i % 5 != 0) ticks_label_y.className = "label_1au_y";
+  else ticks_label_y.className = "label_5au_y";
+  ticks_label_y.textContent = Math.abs(i) + "AU";
+  ticks_label_y.style.backgroundColor = "transparent";
+
+  axis[1].line.layers.enableAll();
+  const Label_y = new THREE.CSS2DObject(ticks_label_y);
+  if (i % 5 != 0) Label_y.position.set(0, i + 0.04, 0);
+  else Label_y.position.set(0, i + 0.5, 0);
+
+  axis[1].line.add(Label_y);
+  Label_y.layers.set(0);
+
+  const ticks_label_z = document.createElement("div");
+
+  if (i % 5 != 0) ticks_label_z.className = "label_1au_z";
+  else ticks_label_z.className = "label_5au_z";
+  ticks_label_z.textContent = Math.abs(i) + "AU";
+  ticks_label_z.style.backgroundColor = "transparent";
+
+  axis[1].line.layers.enableAll();
+  const Label_z = new THREE.CSS2DObject(ticks_label_z);
+  if (i % 5 != 0) Label_z.position.set(0.3, 0, i);
+  else Label_z.position.set(1.3, 0, i);
+
+  axis[1].line.add(Label_z);
+  Label_z.layers.set(0);
+}
+
+console.log();
+// n=0
+// for (let i = -50; i < 50; i++) {
+//     if(i%5==0)continue;
+
+// xticks1[].line.layers.enableAll();
+// const moonLabel = new THREE.CSS2DObject( moonDiv );
+// console.log(moonLabel);
+// moonLabel.position.set( i, 0, 0.25 );
+// moonLabel.layers.set( 0 );
+// xticks1[1].line.add( moonLabel );
+
+// }
 
 function updateLayout() {
   // const angle = cameraDirection.angleTo(lineDirection);
@@ -215,6 +271,7 @@ function updateLayout() {
     w = window.innerWidth;
   }
   renderer.setSize(w, h);
+  labelRenderer.setSize(w, h);
   renderer.render(scene, camera); // レンダリング
   camera.aspect = w / h;
   camera.updateProjectionMatrix();
@@ -223,7 +280,9 @@ function update_camera() {
   const direction = new THREE.Vector3();
   camera.getWorldDirection(direction);
   dist = camera.position.length();
+  axis[0].line.material.opacity = Math.cos(direction.x * 1.5);
   axis[1].line.material.opacity = Math.cos(direction.y * 1.5);
+  axis[2].line.material.opacity = Math.cos(direction.z * 1.5);
 
   //   ang=Math.atan2(camera.position.z,camera.position.x)
   for (let i = 0; i < yticks0_1.length; i++) {
@@ -236,10 +295,12 @@ function update_camera() {
     yticks0_1[i].positions[5] = (-camera.position.x / dist) * 0.05;
     yticks0_1[i].geometry.attributes.position.needsUpdate = true;
 
-    xticks0_1[i].line.material.opacity = 1 - dist * 0.3;
+    xticks0_1[i].line.material.opacity =
+      Math.cos(direction.x * 1.5) - dist * 0.3;
     yticks0_1[i].line.material.opacity =
       Math.cos(direction.y * 1.5) - dist * 0.3;
-    zticks0_1[i].line.material.opacity = 1 - dist * 0.3;
+    zticks0_1[i].line.material.opacity =
+      Math.cos(direction.z * 1.5) - dist * 0.3;
   }
   for (let i = 0; i < yticks1.length; i++) {
     yticks1[i].positions[0] = (-camera.position.z / dist) * 0.2;
@@ -249,10 +310,12 @@ function update_camera() {
     yticks1[i].positions[5] = (-camera.position.x / dist) * 0.2;
     yticks1[i].geometry.attributes.position.needsUpdate = true;
 
-    xticks1[i].line.material.opacity = 1 - dist * 0.06;
+    xticks1[i].line.material.opacity =
+      Math.cos(direction.x * 1.5) - dist * 0.06;
     yticks1[i].line.material.opacity =
       Math.cos(direction.y * 1.5) - dist * 0.06;
-    zticks1[i].line.material.opacity = 1 - dist * 0.06;
+    zticks1[i].line.material.opacity =
+      Math.cos(direction.z * 1.5) - dist * 0.06;
   }
   for (let i = 0; i < yticks5.length; i++) {
     yticks5[i].positions[0] = -camera.position.z / dist;
@@ -260,10 +323,64 @@ function update_camera() {
 
     yticks5[i].positions[3] = camera.position.z / dist;
     yticks5[i].positions[5] = -camera.position.x / dist;
-    yticks5[i].line.material.opacity = Math.cos(direction.y * 1.5);
     yticks5[i].geometry.attributes.position.needsUpdate = true;
+
+    xticks5[i].line.material.opacity = Math.cos(direction.x * 1.5);
+    yticks5[i].line.material.opacity = Math.cos(direction.y * 1.5);
+    zticks5[i].line.material.opacity = Math.cos(direction.z * 1.5);
   }
-  // console.log(direction);
+  au_labels_x = document.getElementsByClassName("label_1au_x");
+  au_labels_y = document.getElementsByClassName("label_1au_y");
+  au_labels_z = document.getElementsByClassName("label_1au_z");
+
+  au_labels_5_x = document.getElementsByClassName("label_5au_x");
+  au_labels_5_y = document.getElementsByClassName("label_5au_y");
+  au_labels_5_z = document.getElementsByClassName("label_5au_z");
+  for (let i = 0; i < au_labels_x.length; i++) {
+    const au_label = au_labels_x[i];
+    au_label.style.setProperty(
+      "--opacity",
+      -Math.abs(direction.x) - dist * 0.04 +1
+    );
+  }
+  for (let i = 0; i < au_labels_y.length; i++) {
+    const au_label = au_labels_y[i];
+    au_label.style.setProperty(
+      "--opacity",
+      -Math.abs(direction.y) - dist * 0.04 +1
+    );
+  }
+  for (let i = 0; i < au_labels_z.length; i++) {
+    const au_label = au_labels_z[i];
+    au_label.style.setProperty(
+      "--opacity",
+      -Math.abs(direction.z) - dist * 0.04 +1
+    );
+  }
+  for (let i = 0; i < au_labels_5_x.length; i++) {
+    const au_label_5 = au_labels_5_x[i];    
+    au_label_5.style.setProperty(
+        "--opacity",
+        1-Math.abs(direction.x) 
+      );
+  }
+  for (let i = 0; i < au_labels_5_y.length; i++) {
+    const au_label_5 = au_labels_5_y[i];
+    au_label_5.style.setProperty(
+      "--opacity",
+      1-Math.abs(direction.y) 
+    );
+  }
+
+  for (let i = 0; i < au_labels_5_z.length; i++) {
+    const au_label_5 = au_labels_5_z[i];
+    au_label_5.style.setProperty(
+      "--opacity",
+      1-Math.abs(direction.z) 
+    );
+  }
+  //   au_label.style.color= rgba( 0, 0, 0,  1 - dist * 0.06 );
+  console.log(direction);
 }
 controls.addEventListener("change", update_camera);
 
@@ -271,6 +388,12 @@ controls.addEventListener("change", update_camera);
 
 // // // ウィンドウリサイズに対応
 window.addEventListener("resize", updateLayout);
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  labelRenderer.render(scene, camera);
+}
+animate();
 // // 例：線を動的に更新
 // setTimeout(() => {
 //     updateLine(0, [
@@ -285,4 +408,3 @@ window.addEventListener("resize", updateLayout);
 //         new THREE.Vector3(-2, -2, 0)
 //     ]);
 // }, 4000);
-
